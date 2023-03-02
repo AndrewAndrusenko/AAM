ee0d15f5b30e763be52b2d544a2df0cef804f574
Andrey Andrusenko <aandrusenko@yandex.ru>
2023-03-03 02:41:38 +0300

    Entries part N

--- src/app/services/app-accounting.service.ts
+++ src/app/services/app-accounting.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable} from 'rxjs';
-import { bcTransactionType_Ext, SWIFTSGlobalListmodel, SWIFTStatement950model } from '../models/accounts-table-model';
+import { Observable, Subject} from 'rxjs';
+import { bAccounts, bcTransactionType_Ext, SWIFTSGlobalListmodel, SWIFTStatement950model } from '../models/accounts-table-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppAccountingService {
   constructor(private http:HttpClient) { }
+  private subjectName = new Subject<any>(); 
 
   GetSWIFTsList (dataRange: string, id: number, MTType:string, Sender: string, Action: string):Observable <SWIFTSGlobalListmodel[]> {
     const params = {'dataRange': dataRange, 'id' :id, 'MTType': MTType,'Sender':Sender, 'Action': Action}
@@ -22,14 +23,34 @@ export class AppAccountingService {
     const params = {'dataRange': dataRange, 'id' :id, 'MTType': MTType,'Sender':Sender, 'Action': Action}
     return this.http.get <bcTransactionType_Ext []>('/api/DEA/fGetAccountingData/', { params: params })
   }
- EntryCreate ():Observable <any[]> {
-  console.log('ftEntryCreate','ftEntryCreate');
+  GetAccountData (currencyCode: number, id: number, clientId:number, accountNo: string, Action: string):Observable <bAccounts[]> {
+    const params = {'currencyCode': currencyCode, 'id' :id, 'clientId': clientId,'accountNo':accountNo, 'Action': Action}
+    return this.http.get <bAccounts []>('/api/DEA/fGetAccountingData/', { params: params })
+  }
+  EntryCreate ():Observable <any[]> {
+    console.log('ftEntryCreate','ftEntryCreate');
     return this.http.get <any []>('/api/DEA/ftEntryCreate/')
   }
   
   GetEntryScheme (bcEntryParameters:any) :Observable <bcTransactionType_Ext[]> { 
     console.log('GetEntryScheme', bcEntryParameters);
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    let params = new URLSearchParams();
+    params.append("someParamKey", '3') 
+
     return this.http.get <bcTransactionType_Ext []>('/api/DEA/GetEntryScheme/', { params: bcEntryParameters })  
   } 
 
+  TGetEntryScheme (bcEntryParameters:any) :Observable <any[]> { 
+    console.log('GetEntryScheme', bcEntryParameters);
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    let params = new URLSearchParams();
+    params.append("someParamKey", '3') 
+
+    return this.http.get <any []>('/api/DEA/GetEntryScheme/', { params: bcEntryParameters })  
+  } 
+  sendEntryDraft (data: any) { this.subjectName.next(data);}
+  getEntryDraft (): Observable<any> {return this.subjectName.asObservable(); }
 }
